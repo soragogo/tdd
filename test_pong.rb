16ecaec096d8393894e404b915bbab4c68d16d78
soragogo <es377233@gmail.com>
2024-09-04 14:59:12 +0900

    🟥パドルを持たせる

--- test_pong.rb
+++ test_pong.rb
@@ -21,4 +21,14 @@ class TestPong < Minitest::Test
     end
     
 
+    def test_pong_has_paddles
+        assert_equal 10, @pong.paddle_width
+        assert_equal 70, @pong.paddle_height
+        assert_equal 10, @pong.paddle_speed
+        assert_equal @pong.height/2, @pong.paddle1_y
+        assert_equal @pong.height/2, @pong.paddle2_y
+        assert_equal 0, @pong.paddle1_x
+        assert_equal @pong.width - 10, @pong.paddle2_x
+    end
+
 end
