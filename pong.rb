c296df35ce12ea8bc778a46683412323230f95c1
soragogo <es377233@gmail.com>
2024-09-04 14:55:32 +0900

    🟢プレイヤーのスコアを持たせる

--- pong.rb
+++ pong.rb
@@ -1,7 +1,9 @@
 class Pong
-    attr_reader :width, :height
+    attr_reader :width, :height, :player1_score, :player2_score
     def initialize
         @width = 800
         @height = 600
+        @player1_score = 0
+        @player2_score = 0
     end
 end
