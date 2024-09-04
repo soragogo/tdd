1b86e49cebe609ff1ffa26b8110c7e750e2305af
soragogo <es377233@gmail.com>
2024-09-04 14:59:22 +0900

    🟢パドルを持たせる

--- pong.rb
+++ pong.rb
@@ -1,9 +1,18 @@
 class Pong
-    attr_reader :width, :height, :player1_score, :player2_score
+    attr_reader :width, :height
+    attr_reader :player1_score, :player2_score
+    attr_reader :paddle_width, :paddle_height, :paddle_speed, :paddle1_y, :paddle2_y, :paddle1_x, :paddle2_x
     def initialize
         @width = 800
         @height = 600
         @player1_score = 0
         @player2_score = 0
+        @paddle_width = 10
+        @paddle_height = 70
+        @paddle_speed = 10
+        @paddle1_y = @height/2
+        @paddle2_y = @height/2
+        @paddle1_x = 0
+        @paddle2_x = @width - 10
     end
 end
